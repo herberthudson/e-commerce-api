2f088054dff5347877ba602510a35d262f5acce4
Herbert Hudson <herberthudson@gmail.com>
2020-11-28 16:56:42 -0300

    init repo

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+export default {
+    "env": {
+        "browser": false,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
