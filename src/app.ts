818a8a5870ae9e476bb1fae3da9ee9aca33a18cb
Herbert Hudson <herberthudson@gmail.com>
2020-11-29 17:42:11 -0300

    add middleware to not found and internal server error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express from 'express'
+import express, { Errback, NextFunction, Request, Response } from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import router from './routes'
+import { StatusCodes, ReasonPhrases } from 'http-status-codes'
 
 const app = express()
 
@@ -10,5 +11,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(router)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.NOT_FOUND).json({ message: ReasonPhrases.NOT_FOUND })
+})
+
+app.use((error: Errback, req: Request, res: Response, next: NextFunction) => {
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ message: ReasonPhrases.INTERNAL_SERVER_ERROR })
+})
 
 export { app }
