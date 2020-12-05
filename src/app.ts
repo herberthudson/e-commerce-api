baf3bbedab1e2e96f8bb121e88dfa08e37af2928
Herbert Hudson <herberthudson@gmail.com>
2020-12-04 21:57:10 -0300

    fix(header): disable x-powered-by vulnerability
    removing vulnerability of recovering fingerprints from web application by disable x-powered-by from
    header

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import { StatusCodes, ReasonPhrases } from 'http-status-codes'
 
 const app = express()
 
+app.disable('x-powered-by')
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
